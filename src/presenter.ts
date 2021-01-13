d1e7cc07620f8f6528cc80dd5656defd5ff598ac
Bart Overwater <bartownsu@gmail.com>
2021-01-13 22:01:57 +0100

    Add doc comments

--- src/presenter.ts
+++ src/presenter.ts
@@ -3,25 +3,26 @@ import * as view from "./view.js";
 import * as model from "./model.js";
 import settings from "./game-settings.js";
 
-// let snake: Snake;
-// const foods: Food[] = []; // voedsel voor de slang
-// let width: number; // breedte van het tekenveld
-// let height: number; // hoogte van het tekenveld
-// let xMax: number; // maximale waarde van x = width - R
-// let yMax: number; // maximale waarde van y = height - R
-// let direction = Direction.Up;
+/**
+ * @module Presenter
+ */
 
 /**
-  @function init() -> void
-  @desc Haal eventueel bestaand voedsel en een bestaande slang weg, cre\"eer een slang, genereer voedsel, en teken alles
-*/
+ * Haal eventueel bestaand voedsel en een bestaande slang weg, cre\"eer een slang, genereer voedsel, en teken alles.
+ *
+ * @param {number} width De width van het speelveld
+ * @param {number} height De height van het speelveld
+ */
 const init = (width: number, height: number) => {
   model.newGame(width, height);
   view.onDirectionChanged(changeDirection);
   eventLoop();
 };
 
-const eventLoop = () => {
+/**
+ * Starts the event loop. The timeout number of seconds can be set in the game-settings.
+ */
+const eventLoop = (): void => {
   if (model.gameOver) {
     return;
   }
@@ -32,144 +33,28 @@ const eventLoop = () => {
   }, settings.SLEEPTIME);
 };
 
+/**
+ * A function to pass the new direction from the view to the model.
+ *
+ * @param {Direction} newDirection
+ */
 const changeDirection = (newDirection: Direction) => {
   model.changeDirection(newDirection);
 };
 
+/**
+ * Main presenter class, used to initialize the code.
+ *
+ * @export
+ * @class Presenter
+ */
 export class Presenter {
+  /**
+   * Initializes the code. This does not start the game.
+   *
+   * @memberof Presenter
+   */
   load(): void {
     view.onStartButtonClicked(init);
   }
 }
-
-// /**
-//   @function move(direction) -> void
-//   @desc Beweeg slang in aangegeven richting
-//         tenzij slang uit canvas zou verdwijnen
-//   @param   {string} direction de richting (een van de constanten UP, DOWN, LEFT of RIGHT)
-// */
-// function move(direction: Direction) {
-//   if (snake.canMove(direction)) {
-//     snake.doMove(direction);
-//     draw();
-//   } else {
-//     console.log("snake cannot move " + direction);
-//   }
-// }
-
-// /**
-//   @function draw() -> void
-//   @desc Teken de slang en het voedsel
-// */
-// function draw() {
-//   const canvas = $("#mySnakeCanvas");
-//   /* in te vullen */
-// }
-// /***************************************************************************
-//  **                 Constructors                                          **
-//  ***************************************************************************/
-// /**
-//    @constructor Snake
-//    @param {[Element] segments een array met aaneengesloten slangsegmenten
-//                    Het laatste element van segments wordt de kop van de slang
-// */
-// function SnakeOud(segments) {
-//   /* in te vullen */
-// }
-// /**
-//    @constructor Element
-//    @param radius straal
-//    @param {number} x x-coordinaat middelpunt
-//    @param {number} y y-coordinaat middelpunt
-//    @param {string} color kleur van het element
-// */
-// function Element(radius, x, y, color) {
-//   /* in te vullen */
-// }
-// /***************************************************************************
-//  **                 Hulpfuncties                                          **
-//  ***************************************************************************/
-
-// /**
-//   @function createStartSnake() -> Snake
-//   @desc Slang creëren, bestaande uit  twee segmenten,
-//         in het midden van het veld
-//   @return: slang volgens specificaties
-// */
-// function createStartSnake() {
-//   const segments: number[] = [
-//     createSegment(R + width / 2, R + height / 2),
-//     createSegment(R + width / 2, height / 2 - R),
-//   ];
-//   snake = new Snake(segments);
-// }
-// /**
-//   @function createSegment(x,y) -> Element
-//   @desc Slangsegment creeren op een bepaalde plaats
-//   @param {number} x x-coordinaat middelpunt
-//   @param {number} y y-coordinaart middelpunt
-//   @return: {Element} met straal R en color SNAKE
-// */
-// function createSegment(x, y) {
-//   return new Element(R, x, y, SNAKE);
-// }
-// /**
-//   @function createFood(x,y) -> Element
-//   @desc Voedselelement creeren op een bepaalde plaats
-//   @param {number} x x-coordinaat middelpunt
-//   @param {number} y y-coordinaart middelpunt
-//   @return: {Element} met straal R en color FOOD
-// */
-// function createFood(x, y) {
-//   return new Element(R, x, y, FOOD);
-// }
-// /**
-//   @function drawElement(element, canvas) -> void
-//   @desc Een element tekenen
-//   @param {Element} element een Element object
-//   @param  {dom object} canvas het tekenveld
-// */
-// function drawElement(element, canvas) {
-//   canvas.drawArc({
-//     draggable: false,
-//     fillStyle: element.color,
-//     x: element.x,
-//     y: element.y,
-//     radius: element.radius,
-//   });
-// }
-
-// /**
-//   @function getRandomInt(min: number, max: number) -> number
-//   @desc Creeren van random geheel getal in het interval [min, max]
-//   @param {number} min een geheel getal als onderste grenswaarde
-//   @param {number} max een geheel getal als bovenste grenswaarde (max > min)
-//   @return {number} een random geheel getal x waarvoor geldt: min <= x <= max
-// */
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// /**
-//   @function createFoods() -> array met food
-//   @desc [Element] array van random verdeelde voedselpartikelen
-//   @return [Element] array met food
-// */
-// function createFoods() {
-//   var i = 0,
-//     food;
-//   //we gebruiken een while omdat we, om een arraymethode te gebruiken, eerst een nieuw array zouden moeten creëren (met NUMFOODS elementen)
-//   while (i < NUMFOODS) {
-//     food = createFood(
-//       XMIN + getRandomInt(0, xMax),
-//       YMIN + getRandomInt(0, yMax)
-//     );
-//     if (
-//       !food.collidesWithOneOf(snake.segment) &&
-//       !food.collidesWithOneOf(foods)
-//     ) {
-//       foods.push(food);
-//       i++;
-//     }
-//   }
-// }
--- src/view.ts
+++ src/view.ts
@@ -2,10 +2,24 @@
 import { Direction } from "./direction.js";
 import { GameModel, Point } from "./model.js";
 import settings from "./game-settings.js";
+/** @module View */
+/**
+ * The Html Canvas Element
+ * @type {HTMLCanvasElement} */
+const canvas: HTMLCanvasElement = document.getElementById(
+  "mySnakeCanvas"
+)! as HTMLCanvasElement;
+/**
+ * The 2d rendering context of the canvas.
+ * @type {CanvasRenderingContext2D}  */
+const canvasContext: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
-const canvas = document.getElementById("mySnakeCanvas")! as HTMLCanvasElement;
-const canvasContext = canvas.getContext("2d")!;
-
+/**
+ * Gets the startSnake button from the DOM and adds onStartFunction to the Click Event Listener.
+ *
+ * @export
+ * @param {function(number, number): void} onStartFunction
+ */
 export function onStartButtonClicked(
   onStartFunction: (width: number, height: number) => void
 ): void {
@@ -16,12 +30,36 @@ export function onStartButtonClicked(
     );
 }
 
+/**
+ * Gets the stopSnake button from the DOM and adds the onStopFunction to the Click Event Listener.
+ *
+ * @export
+ * @param {function(): void} onStopFunction The stop function to call when the stop button is clicked.
+ */
+export function onStopButtonClicked(onStopFunction: () => void): void {
+  document
+    .getElementById("stopSnake")!
+    .addEventListener("click", onStopFunction);
+}
+
+/**
+ * Clears the canvas and then draws the snake and food arrays from the model on the canvas.
+ *
+ * @export
+ * @param {GameModel} model A model to draw on the canvas.
+ */
 export function drawModelOnCanvas(model: GameModel): void {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   model.snake.forEach(drawPoint);
   model.food.forEach(drawPoint);
 }
 
+/**
+ * Adds the given changeDirFunction to the document "keydown" event listener.
+ *
+ * @export
+ * @param  {function(Direction): void} changeDirFunction
+ */
 export function onDirectionChanged(
   changeDirFunction: (newDirection: Direction) => void
 ): void {
@@ -30,6 +68,11 @@ export function onDirectionChanged(
   });
 }
 
+/**
+ * Draws the given point on the canvas.
+ *
+ * @param {Point} point Point to draw on the canvas.
+ */
 const drawPoint = (point: Point): void => {
   const circle = new Path2D();
   circle.arc(point.x, point.y, settings.ELEMENT_RADIUS, 0, 2 * Math.PI, false);
